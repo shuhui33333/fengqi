f77887ddd3dd4e225fef26a74dd341e4fa77aa52
Shuhui <sqiu0011@student.monash.edu>
2026-03-27 15:28:15 +0800

    update services page

---
+++ src/data/navigation.ts
@@ -0,0 +1,26 @@
+export default [
+  { label: "ABOUT", href: "/about" },
+
+  {
+    label: "BUSINESS",
+    href: "/business",
+    children: [
+      { label: "Overview", href: "/business" },
+      { label: "Matrix", href: "/business#matrix" },
+    ],
+  },
+
+  {
+    label: "PLATFORMS",
+    href: "/platforms",
+    children: [
+      { label: "Halal Industrial Park", href: "/platforms#park" },
+      { label: "Supply Chain", href: "/platforms#supply" },
+      { label: "Digital Platform", href: "/platforms#digital" },
+    ],
+  },
+
+  { label: "TECHNOLOGY", href: "/technology" },
+  { label: "LEADERSHIP", href: "/leadership" },
+  { label: "PARTNERSHIP", href: "/partnership" },
+];
